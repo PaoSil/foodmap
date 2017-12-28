71f37e387159f3abafed320226d29af378c3310c
PaoSil <pshuaman@outlook.com>
2017-12-27 23:10:26 -0500

    1ra modificacion de css, html, js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
-$(document).ready(function(){
-  
-});
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 8,
+    center: {lat:-12.0431800, lng:-77.0282400}
+  });
+}
