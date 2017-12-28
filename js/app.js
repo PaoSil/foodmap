d64c09510bf81326c51fc48457f1af268e403120
PaoSil <pshuaman@outlook.com>
2017-12-28 03:49:16 -0500

    2da modificacion de css, js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
-function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 8,
-    center: {lat:-12.0431800, lng:-77.0282400}
-  });
-}
+$(document).ready(function() {
+  setTimeout(function() {
+    window.location.href = 'views/search.html'
+  }, 3000);
+});
---
+++ js/search.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  map = new GMaps ({
+    div: '#map',
+    lat: -12.043333,
+    lng: -77.028333,
+    zoom: 15
+  });
+});
